ead43e7cc0f43fa906732675ff18c39545884a00
Frans <fjaspers@xs4all.nl>
2016-10-30 22:31:20 +0100

    first versin of keu working

--- src/components/Calculator/CalcContainer/CalcContainer.js
+++ src/components/Calculator/CalcContainer/CalcContainer.js
@@ -1,8 +1,7 @@
 "use strict";
 
-import { Component, PropTypes } from "react";
-
 /* eslint-disable no-unused-vars */
+import React, { Component, PropTypes } from "react";
 import CalcDisplay from "../CalcDisplay";
 import CalcKeyboard from "../CalcKeyboard";
 import CalcOperatorboard from "../CalcOperatorboard";
@@ -10,14 +9,33 @@ import CalcOperatorboard from "../CalcOperatorboard";
 
 import s from "./CalcContainer.css";
 import withStyles from "isomorphic-style-loader/lib/withStyles";
+import flatten from "lodash.flatten";
 
 class CalcContainer extends Component {
     constructor(props) {
         super(props);
+
+        this.keyboardKeys = [ ["7", "8", "9"], ["4", "5", "6"], ["1", "2", "3"], ["0", ".", "ce"] ];
+        this.operatorKeys = [ ["=", "+", "-", "*", "/"] ];
+
+        this.handleKeyUp = this.handleKeyUp.bind(this);
+    }
+
+    handleKeyUp(e) {
+        if(flatten(this.operatorKeys).includes(e.key)) {
+            this.props.onOperator(e.key);
+        }
+        if(flatten(this.keyboardKeys).includes(e.key)) {
+            this.props.onKey(e.key);
+        }
+    }
+
+    componentDidMount() {
+        this.calcContainer.focus();
     }
 
     render() {
-        return <div className={s.root}>
+        return <div tabIndex="1" ref={(e) => this.calcContainer = e} onKeyUp={e => this.handleKeyUp(e)} className={s.root}>
             <h1 className="bg-primary">{this.props.title}</h1>
             <div className="row">
                 <div className="col-md-offset-4 col-md-3">
