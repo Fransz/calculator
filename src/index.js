2517b9c036b067c308867d93c92bdcf0b74b9feb
Frans <fjaspers@xs4all.nl>
2016-10-30 09:37:36 +0100

    codingstyle

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-// var css = require('./style.styl')
+// var css = require("./style.styl")
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/App'
-import Calculator from './components/Calculator/Calculator'
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import Calculator from "./components/Calculator/Calculator";
 
 const context = {
-  insertCss: (...styles) => {
-    const removeCss = styles.map(x => x._insertCss())
-    return () => { removeCss.forEach(f => f()); }
-  }
-}
+    insertCss: (...styles) => {
+        const removeCss = styles.map(x => x._insertCss());
+        return () => { removeCss.forEach(f => f())};
+    }
+};
 
-ReactDOM.render(<App context={context}><Calculator /></App>, document.querySelectorAll('#root')[0])
+ReactDOM.render(<App context={context}><Calculator title="calculator" /></App>, document.querySelectorAll("#root")[0]);
